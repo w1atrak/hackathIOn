3b0ad8b55454bf463c17d5eb66884a236deba396
Daniel Bielaszka <danielbielaszka@gmail.com>
2024-05-18 18:27:52 +0200

    try simplified routing in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,9 @@
 "use client";
 
-import {BrowserRouter as Router, Route, Routes, useRoutes} from 'react-router-dom';
-import WelcomePage from './welcome';
-import HomePage from "~/app/home/page";
-import QuizPage from "~/app/quiz/page";
-import ClassSelection from "~/app/class/page";
-import LogInPage from "~/app/login/page";
+import WelcomePage from "./welcome";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<WelcomePage />} />
-                <Route path="/login" element={<LogInPage />} />
-                <Route path="/home" element={<HomePage />} />
-                <Route path="/quiz" element={<QuizPage />} />
-                <Route path="/class" element={<ClassSelection />} />
-            </Routes>
-        </Router>
-    );
+  return <WelcomePage />;
 }
 
 export default App;
